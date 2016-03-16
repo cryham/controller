10289d3561a55283d186406a01412ed12f2d055e
CryHam <cryham@gmail.com>
2016-03-16 19:19:00 +0100

    removed some not needed files from teensy3/

--- teensy3/WProgram.h
+++ teensy3/WProgram.h
@@ -42,8 +42,8 @@ uint16_t makeWord(byte h, byte l);
 
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
 
-void tone(uint8_t pin, uint16_t frequency, uint32_t duration = 0);
-void noTone(uint8_t pin);
+//void tone(uint8_t pin, uint16_t frequency, uint32_t duration = 0);
+//void noTone(uint8_t pin);
 
 // WMath prototypes
 uint32_t random(void);
