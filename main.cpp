3a08006b88ab36719aa882fd3640a74eb4f02900
CryHam <cryham@gmail.com>
2016-04-05 19:41:56 +0200

    main clean timer

--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 
-#include <SPI.h>
-#include <Lib/delay.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define PROGMEM
-#include "FreeSans9pt7b.h"
-
 // hw spi  13 clk  9. d/c  10 cs  11 mosi (dout)
 #define OLED_CS    10
 #define OLED_DC     9
 
 #define KII
-
 #ifdef KII
 extern "C" {
 	#include <Lib/MainLib.h>
@@ -25,19 +17,6 @@ extern "C" {
 #include "demos.h"
 #include "gui.h"
 
-
-//------------------------------
-//#define TIM
-#ifdef TIM
-//#include "IntervalTimer.h"
-//IntervalTimer timer0;
-
-volatile uint32_t t1 = 0;
-
-void timerCallback0()
-{	t1++;
-}
-#endif
 
 Demos demos;
 #ifdef KII
@@ -58,7 +37,6 @@ int main()
 	pinMode(OLED_DC, OUTPUT);
 	pinMode(OLED_CS, OUTPUT);
 	Adafruit_SSD1306 display(OLED_DC, -1, OLED_CS);
-
 	eeprom_initialize();
 
 	demos.Init();
@@ -67,17 +45,18 @@ int main()
 	#ifdef KII
 	gui.LedsInit();  //
 	gui.Load();  // load seq data from eeprom
+	gui.tInfo = 0;
 	#endif
 
 
 	//--------------------------------  loop
 	#define iwait 50  // 2nd reset
-	int anyold = 0;
+	int any_old = 0;
 	int ii = 0;
 	while (1)
 	{
 	
-		#ifdef KII
+		#ifdef KII   //----  kiibohd
 		//  Process CLI
 		//CLI_process();
 
@@ -93,32 +72,28 @@ int main()
 		#endif
 
 		++ii;
-		if (ii == 50)
+		if (ii == iwait)
 		{
 			demos.Reset(display);  // oled init 2nd
-
-			#ifdef TIM
-			//timer0.begin(timerCallback0, 20000);  // 100 Hz
-			#endif
 		}
 		
 		//--------------------------------  display
 		#ifdef KII
-		int any = gui.menu ? 1 : 0;
+		int any = gui.menu;
 		#else
 		int any = 1;
 		#endif
 
 		//  clear on display off
-		if (!any && anyold && ii > iwait)
+		if (!any && any_old && ii > iwait)
 		{
 			display.clearDisplay();
 			display.display();
 		}
 		if (ii > iwait)
-			anyold = any;
+			any_old = any;
 			
-		int demo = gui.menu && gui.mlevel > 0 && !gui.help;
+		int demo = gui.DrawDemo();
 		
 		if (any)
 		{
@@ -129,33 +104,26 @@ int main()
 				#else
 				demos.Draw(display, 1, 1, 8);  //par
 				#endif
-				
-				#ifdef TIM
-				display.setFont(0);
-				display.setCursor(0,0);
-				///display.println(w);  // ee
-				display.println(t1);  // tim1
-				#endif
 			}
 			
 			#ifdef KII
-			gui.Draw(display);  // gui
+			if (gui.menu)
+				gui.Draw(display);  // gui
 			#endif
 
 			display.display();  // show
 		}
 		
-		//if (ii % 300 == 200)  demos.Next();
 		#ifdef KII
 		if (gui.menu)
 		{
 			demos.KeyPress(demo && gui.ym >= MDemos,  gui.ym, gui.ym2[gui.ym]);
-			demos.Dim(display);
+			demos.KeyGlob(display);
 		}
 		gui.KeyPress();
 		gui.ExecSeqs();
 		
-		//  old key states  ---
+		//  old key states  ----
 		for (int i=0; i < 0xFF; ++i)
 			kko[i] = kk[i];
 		#endif
